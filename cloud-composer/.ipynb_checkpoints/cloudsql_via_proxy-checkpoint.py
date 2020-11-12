5d33eff5c7c309bf84c884d2c281af559a7c2644
Johannes Castner <jac2130@gmail.com>
2020-11-12 13:21:21 +0000

    some updates required for our unique work

---
+++ cloud-composer/.ipynb_checkpoints/cloudsql_via_proxy-checkpoint.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+      # -*- coding: utf-8 -*-
+
+from datetime import timedelta, datetime, timezone
+
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+import pymssql
+#import pymssql.cursors
+
+import pendulum
+
+local_tz = pendulum.timezone("Asia/Tokyo")
+
+default_args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2019, 1, 1, tzinfo=local_tz),
+      # 'end_date': datetime(2020, 12, 31),
+    'depends_on_past': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'catchup_by_default': False
+}
+
+dag = DAG('cloudsql_via_proxy', schedule_interval='@once',
+               default_args=default_args)
+
+
+      # Firstly run the dummy operator
+dummy = DummyOperator(
+    task_id='dummy',
+    trigger_rule='all_success',
+    dag=dag,
+)
+
+
+def fn_select():
+    sql = "SELECT * FROM [CLOUDSQL_TABLE]"
+         # connect the Cloud SQl via proxy
+         # put the 'cluster IP' in the 'host'of the proxy pod
+    connection = pymssql.connect(
+        host='[SQLPROXY_CLUSTER_IP]',
+        port=3306,
+        user='[CLOUDSQL_USER]',
+        password='[CLOUDSQL_PASSWORD]',
+        db='[CLOUDSQL_DB]',
+        charset='utf8')
+        #cursorclass=pymssql.cursors.DictCursor)
+
+         # try the SELECT query
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        dbdata = cursor.fetchall()
+        for i in dbdata:
+            print(i)
+        connection.commit()
+
+
+fn_select = PythonOperator(
+         task_id='fn_select',
+         python_callable=fn_select,
+         dag=dag)
+
+
+def fn_insert():
+    sql = "INSERT INTO [CLOUDSQL_TABLE] (col1, col2, col3, col4) VALUES (%s, %s, %s, %s)"
+    connection = pymssql.connect(
+            host='[SQLPROXY_CLUSTER_IP]',
+            port=3306,
+            user='[CLOUDSQL_USER]',
+            password='[CLOUDSQL_PASSWORD]',
+            db='[CLOUDSQL_DB]',
+            charset='utf8')
+            #cursorclass=pymssql.cursors.DictCursor)
+
+         # try the INSERT query
+    with connection.cursor() as cursor:
+        r = cursor.execute(sql, (9999, "insert2", "insert3", "insert4"))
+        connection.commit()
+
+
+fn_insert = PythonOperator(
+         task_id='fn_insert',
+         python_callable=fn_insert,
+         dag=dag)
+
+
+def fn_update():
+    sql = "UPDATE [CLOUDSQL_TABLE] SET col2 = %s WHERE id = %s"
+    connection = pymssql.connect(
+            host='[SQLPROXY_CLUSTER_IP]',
+            port=3306,
+            user='[CLOUDSQL_USER]',
+            password='[CLOUDSQL_PASSWORD]',
+            db='[CLOUDSQL_DB]',
+            charset='utf8')
+            #cursorclass=pymssql.cursors.DictCursor)
+
+         # try the UPDATE query
+    with connection.cursor() as cursor:
+        r = cursor.execute(sql, ("update2", 3))
+        connection.commit()
+
+
+fn_update = PythonOperator(
+         task_id='fn_update',
+         python_callable=fn_update,
+         dag=dag)
+
+#dummy >> fn_select >> fn_insert >> fn_update?
--- cloud-composer/.ipynb_checkpoints/mssql_gcs_dataflow_bigquery_dag_2-checkpoint.py
+++ cloud-composer/.ipynb_checkpoints/mssql_gcs_dataflow_bigquery_dag_2-checkpoint.py
@@ -19,15 +19,11 @@ import os
 from airflow import configuration
 from airflow import models
 from airflow.contrib.hooks import gcs_hook
-from airflow.contrib.operators import mssql_to_gcs
+from airflow.contrib.operators import dataflow_operator
 from airflow.operators import python_operator
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.operators import email_operator
-from google.cloud import storage
 
-client = storage.Client()
-bucket = client.get_bucket('us-central1-shared-logic-en-4c9cc71e-bucket')
-blob = storage.Blob('dags/sql_queries/mrrecvh_mrrecvd_ppprice_lj.sql', bucket)
 # We set the start_date of the DAG to the previous date. This will
 # make the DAG immediately available for scheduling.
 YESTERDAY = datetime.datetime.combine(
@@ -38,47 +34,58 @@ YESTERDAY = datetime.datetime.combine(
 SUCCESS_TAG = 'success'
 FAILURE_TAG = 'failure'
 
-DATE = '{{ ds }}'
+DS_TAG = '{{ ds }}'
+DATAFLOW_FILE = os.path.join(
+    configuration.get('core', 'dags_folder'), 'dataflow', 'process_json.py')
 
 DEFAULT_DAG_ARGS = {
     'start_date': YESTERDAY,
+    'email': models.Variable.get('email'),
+    'email_on_failure': True,
+    'email_on_retry': False,
     'retries': 0,
-    'project_id': models.Variable.get('gcp_project')
+    'project_id': models.Variable.get('gcp_project'),
+    'dataflow_default_options': {
+        'project': models.Variable.get('gcp_project'),
+        'temp_location': models.Variable.get('gcp_temp_location'),
+        'runner': 'DataflowRunner'
+    }
 }
 
-
-query1 = blob.download_as_string
-    
-with models.DAG(dag_id='mssql_gcs_dataflow_bigquery_dag_1',
+# Setting schedule_interval to None as this DAG is externally trigger by a Cloud Function.
+with models.DAG(dag_id='mssql_gcs_dataflow_bigquery_dag_2',
                 description='A DAG triggered by an external Cloud Function',
                 schedule_interval=None, default_args=DEFAULT_DAG_ARGS) as dag:
-    # Export task that will process SQL statement and save files to Cloud Storage.
-    export_sales_orders = mssql_to_gcs.MsSqlToGoogleCloudStorageOperator(
-        task_id='mrrecvh_mrrecvd_ppprice_lj',
-        sql=query1,
-        bucket=models.Variable.get('mssql_export_bucket'),
-        filename=DATE + '-export.json',
-        mssql_conn_id='shapiro-sql',
-        dag=dag
-    )
+    # Args required for the Dataflow job.
+    job_args = {
+        'input': 'gs://{{ dag_run.conf["bucket"] }}/{{ dag_run.conf["name"] }}',
+        'output': models.Variable.get('bq_output_table'),
+        'load_dt': DS_TAG
+    }
+
+    # Main Dataflow task that will process and load the input delimited file.
+    dataflow_task = dataflow_operator.DataFlowPythonOperator(
+        task_id="process-json-to-dataflow",
+        py_file=DATAFLOW_FILE,
+        options=job_args)
 
     # Here we create two conditional tasks, one of which will be executed
     # based on whether the export_sales_orders was a success or a failure.
     success_move_task = email_operator.EmailOperator(task_id='success',
                                                     	trigger_rule=TriggerRule.ALL_SUCCESS,
 														to=models.Variable.get('email'),
-														subject='mssql_gcs_dataflow_bigquery_dag_1 Job Succeeded: start_date {{ ds }}',
+														subject='mssql_gcs_dataflow_bigquery_dag_2 Job Succeeded: start_date {{ ds }}',
 														html_content="HTML CONTENT"
 														)
 
     failure_move_task = email_operator.EmailOperator(task_id='failure',
                                                     	trigger_rule=TriggerRule.ALL_FAILED,
 														to=models.Variable.get('email'),
-														subject='mssql_gcs_dataflow_bigquery_dag_1 Job Failed: start_date {{ ds }}',
+														subject='mssql_gcs_dataflow_bigquery_dag_2 Job Failed: start_date {{ ds }}',
 														html_content="HTML CONTENT"
 														)
 
     # The success_move_task and failure_move_task are both downstream from the
     # dataflow_task.
-    export_sales_orders >> success_move_task
-    export_sales_orders >> failure_move_task
+    dataflow_task >> success_move_task
+    dataflow_task >> failure_move_task
